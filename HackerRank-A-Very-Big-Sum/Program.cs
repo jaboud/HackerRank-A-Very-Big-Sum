b6836b0724e20d2d78a96089ba1852c662535e92
jaboud <jaboud94@gmail.com>
2019-09-11 15:04:57 +1000

    Created static method to use arrays.

--- HackerRank-A-Very-Big-Sum/Program.cs
+++ HackerRank-A-Very-Big-Sum/Program.cs
@@ -4,6 +4,11 @@ namespace HackerRank_A_Very_Big_Sum
 {
     class Program
     {
+        //A static method using the long data type using arrays.
+        static long AVeryBigSum(long[] ar)
+        {
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
