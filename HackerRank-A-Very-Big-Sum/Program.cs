11ccac7b2c5d723839e6fa298369f74ff3b5d4e2
jaboud <jaboud94@gmail.com>
2019-09-11 15:36:57 +1000

    Created for loop for the incrementation of the long sums

--- HackerRank-A-Very-Big-Sum/Program.cs
+++ HackerRank-A-Very-Big-Sum/Program.cs
@@ -9,6 +9,12 @@ namespace HackerRank_A_Very_Big_Sum
         {
             //Variable called sum and preset the default value at 0.
             long sum = 0;
+            //For loop when you add the first line of input. For example on hackerrank, if we input "5" as the first line
+            //then we will have to calculate 5 long sums which will be incremented at the end and add them all up and returning the answer.  
+            for (int i = 0; i < ar.Length; i++)
+            {
+                sum += ar[i];
+            }
 
         }
         static void Main(string[] args)
