1443b423d70855f41c14cbecfa74c281cb2289ae
jaboud <jaboud94@gmail.com>
2019-09-11 15:07:22 +1000

    Created sum variable.

--- HackerRank-A-Very-Big-Sum/Program.cs
+++ HackerRank-A-Very-Big-Sum/Program.cs
@@ -7,6 +7,8 @@ namespace HackerRank_A_Very_Big_Sum
         //A static method using the long data type using arrays.
         static long AVeryBigSum(long[] ar)
         {
+            //Variable called sum and preset the default value at 0.
+            long sum = 0;
 
         }
         static void Main(string[] args)
